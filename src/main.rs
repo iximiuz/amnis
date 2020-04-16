ca6500677cfb4291ff6780236d59c4c017c9120b
Ivan Velichko <iximiuz@gmail.com>
2020-04-16 09:20:13 +0200

    up

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ use amnis::output::{LineWriter, Output};
 //   Read input line by line
 //   Parse lines to hashmaps
 
-// amnis -d 're:(\d+) (\w) (\d+) (\d{2})' -t 1 -l word:2 -l num:3:i -f uid:4 -e influxdb:line_proto
+// amnis -d 're:(\d+) (\w) (\d+) (\d{2})' -t timestamp:$1:%Y-%m-%dT%H:%I:%S -l word:$2 -l num:$3:i -f uid:$4 -e influxdb:line_proto
 // amnis -d json -e influxdb:line_proto
 // amnis -d prom -e json
 //
@@ -29,6 +29,15 @@ use amnis::output::{LineWriter, Output};
 //                   :f - float
 //                   :j - json
 //                   :s - string (default, optional)
+//
+//
+// tail -f /var/log/app.log | amnis \
+//   -i '{"re": "(\d+)\s(\w)", "multiline": {"re": "\d4-\d2\d2", "negative": true}, "t:ts": "$1:%Y-%m-%dT%H:%I:%S", "l:num": "$1:i"}' \
+//   -s '{"name": "foo:bar", "query": "sum($input:0.someField)"}' \
+//   -s '{"name": "baz:abc", "transform": "... jq-alike program ..."}' \
+//   -s '{"name": "baz:qux", "query": "foo:bar - baz:abc.otherField"}' \
+//   -o '{"encode": "json"}' \
+//   -o '{"encode": "influxdb.line_proto", "streams": ["foo:bar", "baz:*"], "file": "/var/log/foobarbaz.log"}'
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "amnis", about = "amnis command line arguments")]
